55260749fd54328c3cfd1ff127bc1dec2cbdeed1
kuttyselva <kuttyselva57@gmail.com>
2018-10-11 23:56:08 +0530

    cbv

--- cbv/acbv/acbv/urls.py
+++ cbv/acbv/acbv/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basicapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.Indexview.as_view())
 ]
--- cbv/acbv/basicapp/views.py
+++ cbv/acbv/basicapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import View,TemplateView
+from django.http import HttpResponse
+class Indexview(TemplateView):
+    template_name='index.html'
+    def get_context_data(self,**kwargs):
+        context= super().get_context_data(**kwargs)
+        context['injected']='basic injuction'
+        return context
